e06002e647aeb58c7f1553f2e2adc41e43f2ca9a
Jano Simas <janosimas@gmail.com>
2019-01-01 07:50:44 -0200

    gem as colored squares

--- src/modules/splendor/react-components/gem.js
+++ src/modules/splendor/react-components/gem.js
@@ -5,6 +5,11 @@ export const RenderGem = {};
 
 for (const key in GEM) {
   if (GEM.hasOwnProperty(key)) {
-    RenderGem[GEM[key]] = <div>{GEM[key]}</div>;
+    RenderGem[GEM[key]] = <div style={{
+      width: 20,
+      height: 20,
+      backgroundColor: GEM[key],
+      margin: 7
+    }} />;
   }
 }
