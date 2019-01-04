5b402c77117edab3da862eaf5c81e542ca0908b6
Jano Simas <janosimas@gmail.com>
2019-01-04 11:13:47 -0200

    remove single player and spectator

--- src/modules/splendor/routes.js
+++ src/modules/splendor/routes.js
@@ -6,25 +6,13 @@
  * https://opensource.org/licenses/MIT.
  */
 
-import Singleplayer from './components/singleplayer';
 import Multiplayer from './components/multiplayer';
-import Spectator from './components/spectator';
 
 const routes = [
-  {
-    path: '/splendor',
-    text: 'Singleplayer',
-    component: Singleplayer,
-  },
   {
     path: '/splendor/multiplayer',
     text: 'Multiplayer',
     component: Multiplayer,
-  },
-  {
-    path: '/splendor/spectator',
-    text: 'Spectator',
-    component: Spectator,
   },
 ];
 
