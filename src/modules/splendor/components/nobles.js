5e307cbd721dddae35ac1640b0ba34d81d16735b
Jano Simas <janosimas@gmail.com>
2019-01-05 09:57:44 -0200

    Nobles cards

---
+++ src/modules/splendor/components/nobles.js
@@ -0,0 +1,19 @@
+import { GEM } from "./gems";
+
+export const nobles = [
+  { points: 3, [GEM.BLACK]: 3, [GEM.BLUE]: 3, [GEM.WHITE]: 3 },
+  { points: 3, [GEM.GREEN]: 3, [GEM.BLUE]: 3, [GEM.WHITE]: 3 },
+  { points: 3, [GEM.GREEN]: 3, [GEM.BLUE]: 3, [GEM.RED]: 3 },
+  { points: 3, [GEM.BLACK]: 3, [GEM.RED]: 3, [GEM.GREEN]: 3 },
+  { points: 3, [GEM.BLACK]: 3, [GEM.RED]: 3, [GEM.WHITE]: 3 },
+  { points: 3, [GEM.BLACK]: 4, [GEM.WHITE]: 4 },
+  { points: 3, [GEM.GREEN]: 4, [GEM.RED]: 4 },
+  { points: 3, [GEM.BLUE]: 4, [GEM.GREEN]: 4 },
+  { points: 3, [GEM.BLACK]: 4, [GEM.RED]: 4 },
+  { points: 3, [GEM.BLUE]: 4, [GEM.WHITE]: 4 },
+
+  { points: 3, [GEM.BLUE]: 3, [GEM.WHITE]: 3, [GEM.RED]: 3 },
+  { points: 3, [GEM.BLACK]: 3, [GEM.RED]: 3, [GEM.BLUE]: 3 },
+  { points: 3, [GEM.WHITE]: 4, [GEM.RED]: 4 },
+  { points: 3, [GEM.GREEN]: 4, [GEM.BLACK]: 4 },
+]
