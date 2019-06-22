7e497d09e9786487303a48829b8e1e9f69058357
janosimas <janosimas@gmail.com>
2019-06-22 04:24:51 +0200

    reorganize code

--- src/modules/language-game/game.js
+++ src/modules/language-game/game.js
@@ -13,53 +13,10 @@ import { isNil, isEmpty, uniq, contains, map } from "ramda";
 
 import { default as german } from "./languages/de-de.js";
 
-import request from "sync-request";
+import { getTranslation, getImages } from "./components/requests";
+
 import dotenv from "dotenv";
 dotenv.config();
-
-const getImages = wordItem => {
-  const imageSearchString =
-    isNil(wordItem.search_aid) || isNil(wordItem.search_aid.image)
-      ? wordItem.word
-      : wordItem.search_aid.image;
-
-  const requestOptions = {
-    key: process.env.PIXABAY_KEY,
-    per_page: 4,
-    q: imageSearchString
-  };
-
-  let requestUrl = "https://pixabay.com/api/?";
-  for (const key in requestOptions) {
-    const element = requestOptions[key];
-    requestUrl += key + "=" + element + "&";
-  }
-
-  var res = request("GET", requestUrl);
-  var body = JSON.parse(res.getBody("utf8"));
-
-  return body.hits.map(obj => obj.webformatURL);
-};
-
-const getTranslation = word => {
-  const requestOptions = {
-    key: process.env.YANDEX_KEY,
-    lang: "de-en",
-    format: "plain",
-    text: word
-  };
-
-  let requestUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?";
-  for (const key in requestOptions) {
-    const element = requestOptions[key];
-    requestUrl += key + "=" + element + "&";
-  }
-
-  var res = request("GET", requestUrl);
-  var body = JSON.parse(res.getBody("utf8"));
-
-  return body.text;
-};
 
 const getCompleteTextFromWord = item => {
   let completeText = "";
